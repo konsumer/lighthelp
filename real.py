eba04660206586927ba5162f4a75d77ff5fb116c
David Konsumer <konsumer@jetboystudio.com>
2021-07-09 16:46:21 -0700

    new MultiLight implemantation

--- real.py
+++ real.py
@@ -33,48 +33,33 @@ def get_light(name):
         raise Exception(f"'{name}' light not found.")
     return light
 
-
-# this kind of button toggles 1 light on short-press, and dims on long-press
-class LightButton(ButtonDevice):
-    def __init__(self, id, name, debounce=1, long_time=4):
+# amps a single switch to multiple tinytuya light devices
+class MultipleLightButton(ButtonDevice):
+    def __init__(self, id, *lights):
+        debounce = 250
+        long_time = 2000
         ButtonDevice.__init__(self, id, debounce, long_time)
-        self.light = get_light(name)       
-
-    def short_press(self):
-        dt = datetime.now().strftime('%r %d/%m/%Y')
-        print(f"{dt}: SHORT")
-        data = self.light.status()
-        if(data['dps']['20'] == True):
-            self.light.turn_off()
-        else:
-            self.light.turn_on()
-    
-    # TODO: workout long action
-
-# this kind of button turns all lights off on a short-press
-class AllOffButton(ButtonDevice):
-    def __init__(self, id, lights, debounce=1):
-        ButtonDevice.__init__(self, id, debounce)
         self.lights = {}
         for name in lights:
             self.lights[name] = get_light(name)
     
     def short_press(self):
         dt = datetime.now().strftime('%r %d/%m/%Y')
+        print(f"{dt}: SHORT")
         for name in self.lights:
-            print(f"{dt}: {name} - Off")
-            self.lights[name].turn_off()
-
+            data = self.lights[name].status()
+            if (data['dps']['20'] == True):
+                self.lights[name].turn_off()
+            else:
+                self.lights[name].turn_on()
+        
+        
 
 # this is all the buttons we want to listen to
 rooms = [
-    LightButton(818562),                                         # hallway
-    # LightButton(3764961, "Light_1"),                           # bigg
-    # LightButton(835186, "Light_2"),                            # entrance
-    # LightButton(3764962, "Leo's Light"),                       # leo_room
-    # LightButton(3764964, "Light_3"),                           # my_room
-    
-    # AllOffButton(3764968, ["Light_1", "Light_2", "Light_3"]),  # all_off
+    MultipleLightButton(3764962, "Leo's Light"),
+    MultipleLightButton(835186, "Light_1", "Light_2", "Light_3"), # entrance
+    MultipleLightButton(818562, "Light_1", "Light_2", "Light_3")  # hallway
 ]
 
 # this is the main loop that calls process on all the buttons
