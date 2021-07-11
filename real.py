346d8c04eb3f08dc3d4484ee9f9666550637b961
David Konsumer <konsumer@jetboystudio.com>
2021-07-11 13:37:36 -0700

    helping with real

--- real.py
+++ real.py
@@ -16,29 +16,33 @@ rfdevice = RFDevice(gpio=27)
 rfdevice.enable_rx()
 
 # cleanup on exit
+
+
 def signal_handler(sig, frame):
     rfdevice.cleanup()
     sys.exit(0)
+
+
 signal.signal(signal.SIGINT, signal_handler)
 
 # get a light by name
+
+
 def get_light(name):
     light = None
-    try:
-        for item in jdata["devices"]:
-            if item["name"] == name:
-                light = tinytuya.BulbDevice(item["id"], item["ip"], item["key"])
-                light.set_version(float(item["ver"])) # TODO: check if this needs to be converted to float
-                light.set_socketPersistent(True)
-            if not light:
-                raise Exception(f"'{name}' light not found.")
-    except OSError as err:
-        print("OS error: {0}".format(err))
-        exit
-        
+    for item in jdata["devices"]:
+        if item["name"] == name:
+            light = tinytuya.BulbDevice(item["id"], item["ip"], item["key"])
+            # TODO: check if this needs to be converted to float
+            light.set_version(float(item["ver"]))
+            light.set_socketPersistent(True)
+        if not light:
+            raise Exception(f"'{name}' light not found.")
     return light
 
 # maps a single switch to multiple tinytuya light devices
+
+
 class MultipleLightButton(ButtonDevice):
     def __init__(self, id, *lights):
         debounce = 1000
@@ -48,71 +52,66 @@ class MultipleLightButton(ButtonDevice):
         self.statuses = {}
         self.pressing = False
         self.fade_up = True
-#        self.set_brightness_percentage = {} #broke it ?
-#        self.set_colourtemp_percentage = {} #broke it ?
+
         for name in lights:
-            self.lights[name] = get_light(name)
-    
+            try:
+                self.lights[name] = get_light(name)
+            except:
+                print(f"Light not found: {name}")
+
     def update_status(self):
         self.statuses = {}
         for name in self.lights:
-           self.statuses[name] = self.lights[name].status()
-    
+            try:
+                self.statuses[name] = self.lights[name].status()
+            except:
+                print(f"Failed to update status on {name}")
+
     def short_press(self):
         dt = datetime.now().strftime('%r %d/%m/%Y')
-        print(f"{dt} ({self.id}): SHORT") # how to change print id number to words ie hallway
+        # how to change print id number to words ie hallway
+        print(f"{dt} ({self.id}): SHORT")
         self.update_status()
         for name in self.lights:
             # Edit for the other version of lights, I just added an 'or' on line 61
             # if you really need parallel on/off see https://stackoverflow.com/questions/7207309/how-to-run-functions-in-parallel
-            if (self.statuses[name]['dps']['20'] or ['1'] == True):
+
+            if (self.statuses[name]['dps']['20'] == True):
                 self.lights[name].turn_off()
             else:
                 self.lights[name].turn_on()
-    
+
     def long_rolling(self):
         """ called when button is pressed for a long time, in a rolling manner"""
-        # commented so longpress is clearer
-        # dt = datetime.now().strftime('%r %d/%m/%Y')
-        # print(f"{dt} ({self.id}): ROLLING")
-
         if not self.pressing:
             # one-time code goes here, runs at start of long press
             self.pressing = True
             self.update_status()
-        
-        # TODO: lookup current fade however you do that, from self.statuses
-        # set new fade to some relationship between current fade and self.elapsed
-        
-        # Warning: all pseudo-code below
-        # imagine self.statuses[name]['fade'] is a value from 0-100
-        # and holds current value
+
+        # set new fade to relationship between current-fade and self.elapsed
+
         for name in self.lights:
-            amountToFade = self.elapsed / 2000
-            if self.fade_up:
-                newfade = self.lights[name].set_brightness_percentage(+amountToFade)
-                newfade = self.lights[name].set_colourtemp_percentage(+amountToFade)              
-            else:
-                newfade = self.lights[name].set_brightness_percentage(-amountToFade)
-                newfade = self.lights[name].set_colourtemp_percentage(-amountToFade)
-                if newfade > 100:
-                    newfade = 100
-                if newfade < 0:
-                    newfade = 0
-                self.statuses[name]['fade'] = newfade    
-                self.lights[name].set_brightness_percentage(newfade)    
-                
-#                #.set_brightness_percentage(0-100)
-#                newfade = self.statuses[name]['set_brightness_percentage'] + amountToFade
-#            else:
-#                newfade = self.statuses[name].set_brightness_percentage - amountToFade
-#            if newfade > 100:
-#                newfade = 100
-#            if newfade < 0:
-#                newfade = 0
-#            self.statuses[name]['fade'] = newfade
-#            self.lights[name].set_brightness_percentage(newfade)
-    
+            try:
+                amountToFade = self.elapsed / 2000
+                if self.fade_up:
+                    newfade = self.lights[name].set_brightness_percentage(
+                        +amountToFade)
+                    newfade = self.lights[name].set_colourtemp_percentage(
+                        +amountToFade)
+                else:
+                    newfade = self.lights[name].set_brightness_percentage(
+                        -amountToFade)
+                    newfade = self.lights[name].set_colourtemp_percentage(
+                        -amountToFade)
+                    if newfade > 100:
+                        newfade = 100
+                    if newfade < 0:
+                        newfade = 0
+                    self.statuses[name]['fade'] = newfade
+                    self.lights[name].set_brightness_percentage(newfade)
+            except:
+                print(f"Could not fade {name}")
+
     def long_press(self):
         dt = datetime.now().strftime('%r %d/%m/%Y')
         print(f"{dt} ({self.id}): LONG")
@@ -122,14 +121,9 @@ class MultipleLightButton(ButtonDevice):
 
 # this is all the buttons we want to listen to
 rooms = [
-#    MultipleLightButton(3764961, "Leo's Light"),
-    MultipleLightButton(835186, "Light_1", "Light_2", "Light_3"), # entrance
+    #    MultipleLightButton(3764961, "Leo's Light"),
+    MultipleLightButton(835186, "Light_1", "Light_2", "Light_3"),  # entrance
     MultipleLightButton(818562, "Light_1", "Light_2", "Light_3")  # hallway
-# I have these buttons left over. Dont know what todo with them, 
-# maybe some sort of fade selector 
-#    Button3 = '3764962'
-#    Button4 = '3764964'
-#    Button5 = '3764968'
 ]
 
 # this is the main loop that calls process on all the buttons
